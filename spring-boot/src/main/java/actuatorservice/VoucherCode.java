7ec6f1bf9cb2e9aa21f7d81648a87f786ffe2a9e
Andy Lee <andy.mt.lee@gmail.com>
2019-11-15 12:48:55 +0800

    completed VoucherCode class

--- spring-boot/src/main/java/actuatorservice/VoucherCode.java
+++ spring-boot/src/main/java/actuatorservice/VoucherCode.java
@@ -4,8 +4,33 @@ import java.time.LocalDate;
 
 public class VoucherCode {
 
+    private Recipient recipient;
+    private Offer offer;
+    private LocalDate expiryDate;
+    private long randomCode;
 
     public VoucherCode(long randomCode, Recipient recipient, Offer offer, LocalDate expiryDate) {
 
+        this.recipient = recipient;
+        this.offer = offer;
+        this.expiryDate = expiryDate;
+        this.randomCode = randomCode;
+
+    }
+
+    public Recipient getRecipient() {
+        return this.recipient;
+    }
+
+    public Offer getOffer() {
+        return this.offer;
+    }
+
+    public LocalDate getExpiryDate() {
+        return this.expiryDate;
+    }
+
+    public long getRandomCode() {
+        return this.randomCode;
     }
 }
--- spring-boot/src/test/java/actuatorservice/VoucherCodeTest.java
+++ spring-boot/src/test/java/actuatorservice/VoucherCodeTest.java
@@ -2,15 +2,37 @@ package actuatorservice;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.time.LocalDate;
 
 public class VoucherCodeTest {
 
-    private static final VoucherCode VOUCHER_CODE = new VoucherCode(12345678, new Recipient("Kevin de Bruyne","kevindebruyne@mancity.com"),
-            new Offer("Kevin de Bruyne",0.2d), LocalDate.now());
+    private static final VoucherCode VOUCHER_CODE = new VoucherCode(12345678, new Recipient("Kevin de Bruyne", "kevindebruyne@mancity.com"),
+            new Offer("Kevin de Bruyne", 0.2d), LocalDate.of(2019, 11, 1));
 
     @Test
     public void gettersShouldReturnCorrectResults() {
 
+        final long randomCode = 12345678;
+        final String recipientName = "Kevin de Bruyne";
+        final String email = "kevindebruyne@mancity.com";
+
+        final double percentageDiscount = 0.2d;
+
+        final LocalDate novemberFirstTwentyNineteen = LocalDate.of(2019, 11, 1);
+
+        assertThat(VOUCHER_CODE.getRandomCode(), is(randomCode));
+
+        assertThat(VOUCHER_CODE.getRecipient().getName(), is(recipientName));
+        assertThat(VOUCHER_CODE.getRecipient().getEmail(), is(email));
+
+        assertThat(VOUCHER_CODE.getOffer().getName(), is(recipientName));
+        assertThat(VOUCHER_CODE.getOffer().getPercentageDiscount(), is(percentageDiscount));
+
+        assertThat(VOUCHER_CODE.getExpiryDate(), is(novemberFirstTwentyNineteen));
+
     }
+
 }
