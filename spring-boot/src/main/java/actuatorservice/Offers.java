dc99d7f79d9216d279ef663025565eeb80065bf8
Andy Lee <andy.mt.lee@gmail.com>
2019-11-17 21:44:08 +0800

    added Offers.json

---
+++ spring-boot/src/main/java/actuatorservice/Offers.java
@@ -0,0 +1,32 @@
+package actuatorservice;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.commons.lang3.builder.ToStringStyle.*;
+
+
+public class Offers {
+
+    private List<Offer> offers;
+
+    public Offers(@JsonProperty("offers") List<Offer> offers) {
+
+        this.offers = new ArrayList<>(offers);
+    }
+
+
+    public List<Offer> getOffers() {
+        return this.offers;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, JSON_STYLE)
+                .append("offers", offers)
+                .toString();
+    }
+}
---
+++ spring-boot/src/test/java/actuatorservice/OffersTest.java
@@ -0,0 +1,57 @@
+package actuatorservice;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.io.File;
+
+import static java.nio.charset.Charset.defaultCharset;
+import static org.apache.commons.io.FileUtils.readFileToString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static java.util.Arrays.*;
+
+
+public class OffersTest {
+
+    private static final Offers OFFERS = new Offers(asList(new Offer("BOGOF", 50.00d),
+            new Offer("TEN PERCENT", 10.0d), new Offer("TWENTY PERCENT", 20.00d)));
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    @Test
+    public void gettersShouldReturnCorrectResults() {
+
+        final String offerOne = "BOGOF";
+        final String offerTwo = "TEN PERCENT";
+        final String offerThree = "TWENTY PERCENT";
+
+        final Offer bogof = OFFERS.getOffers().get(0);
+        assertThat(bogof.getName(), is(offerOne));
+        assertThat(bogof.getPercentageDiscount(), is(50.0d));
+        final Offer tenPercent = OFFERS.getOffers().get(1);
+        assertThat(tenPercent.getName(), is(offerTwo));
+        assertThat(tenPercent.getPercentageDiscount(), is(10.0d));
+
+        final Offer twentyPercent = OFFERS.getOffers().get(2);
+        assertThat(twentyPercent.getName(), is(offerThree));
+        assertThat(twentyPercent.getPercentageDiscount(), is(20.0d));
+
+    }
+
+    @Test
+    public void shouldUnmarshallCorrectly() throws Exception {
+
+        final String json = readFileToString(new File("src/main/resources/offers.json"),
+                defaultCharset());
+
+        final Offers expected = OBJECT_MAPPER.readValue(json, Offers.class);
+
+        final String actualString = OFFERS.toString();
+        final String expectedString = expected.toString();
+
+        assertThat(actualString, is(expectedString));
+
+    }
+
+}
